b8c62f14f7d0c4fea567aec1e9702a7aa4ba19d2
Saad <124282322+adnanadilalpha@users.noreply.github.com>
2023-04-05 13:37:52 +0500

    buged fixed

--- pages/components/main/Clients.jsx
+++ pages/components/main/Clients.jsx
@@ -4,7 +4,7 @@ const Clients = () => {
   return (
     <div className=''>
             <div className='mt-32'>
-    <div className="border-t border-b border-slate-400 w-full px-8 py-8 xl:px-40 xl:py-16">
+    <div className="border-t border-b border-neutral-700 w-full px-8 py-8 xl:px-40 xl:py-16">
     <div className="flex flex-col md:flex-row md:gap-y-8 items-center justify-center 2xl:gap-x-20 md:gap-x-8">
 <div className='flex justify-between gap-x-6 xl:gap-x-20'>
 <img src="../images/gs1.svg" alt="" className=""/>
