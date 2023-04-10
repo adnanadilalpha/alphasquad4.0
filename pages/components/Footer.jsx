2f9afb41e63c2701f0746ecea62cf665c8a95792
Saad <124282322+adnanadilalpha@users.noreply.github.com>
2023-04-10 16:44:41 +0500

    more bugs fixed

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
 <div className="flex flex-col justify-between md:flex  2xl:flex  xl:flex-row 2xl:mt-24 mt-8 mb-9 mx-6 2xl:mx-20 font ">
-        <div className="mb-8 flex flex-col items-center md:items-start">
+        <div className="mb-8 flex flex-col items-center md:items-start w-full">
         <span className="text-base sm:text-2xl text-zinc-400">
             FIND US
         </span>
@@ -23,7 +23,7 @@ const Footer = () => {
 <a href='https://www.instagram.com/alphasquadtech/?hl=en' target="_blank"><li className="insta ">INSTAGRAM</li></a>
        </ul>
         </div>
-<div className="flex flex-col md:flex md:flex-row md:justify-between gap-x-0 md:gap-x-28">
+<div className="flex flex-col md:flex md:flex-row md:justify-between gap-x-0 md:gap-x-28 w-full">
          <div className='mb-8 text-center md:text-left'>
          <p className="text-zinc-400 text-sm sm:text-2xl font font-light">
                          UP CLOSE AND PERSONAL
