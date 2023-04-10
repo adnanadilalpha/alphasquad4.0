10a5f7fb9dffbc1c6a7d6c86cfc06c73308db515
Saad <124282322+adnanadilalpha@users.noreply.github.com>
2023-04-10 12:11:20 +0500

    update

--- pages/components/alphaway/component/Animatealpha.jsx
+++ pages/components/alphaway/component/Animatealpha.jsx
@@ -10,7 +10,7 @@ const animatealpha = () => {
         </div>
 
         <div class="logocenter">
-        <svg className="fill-black xl:fill-white" width="62" height="54" viewBox="0 0 62 54" xmlns="http://www.w3.org/2000/svg">
+        <svg className="fill-white" width="62" height="54" viewBox="0 0 62 54" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M31 15.9713L13.519 46.2151H22.8742L18.4332 54H0L30.6931 0.534654L31 0L31.3069 0.534654L62 54H43.5668L39.1258 46.2151H48.481L31 15.9713Z"/>
 </svg>
 
