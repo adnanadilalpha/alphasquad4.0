6ff222fa28b1b26cdf1ede13a1398c9e85c2a2ec
adnanadilalpha <adnanadil.alphasquad@gmail.com>
2023-04-11 21:54:06 +0500

    dope stuff added

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import '../styles/main.css'
 import '../pages/components/alphaway/alphaway.css'
 import '../pages/components/people/people.css'
+import '../pages/components/dope/dope.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/components/dope/component/Flex.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Flex = () => {
+  return (
+<div className="main">
+    <div className="flex-wrap-dope">
+               <div className="flex-item1">
+               </div>
+            <div className="flex-item2">
+            </div>
+              <div className="flex-item3">
+              </div>
+                <div className="flex-item4">
+                </div>
+        <div className="flex-item5">
+        </div>
+              <div className="flex-item6">
+              </div>
+              <div className="flex-item7">
+              </div>
+              <div className="flex-item8">
+              </div>
+              <div className="flex-item9">
+              </div>
+              <div className="flex-item10">
+              </div>
+        </div>
+        <div className="header">
+              <p className="headline--dope">
+              Dope stuff
+              </p>
+            <div>
+               <p className="text-white text-5xl uppercase font-bold">
+               great stuff - for great people
+               </p>
+            </div>
+     </div>   
+    </div>
+
+  )
+}
+
+export default Flex
