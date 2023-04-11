c1d8a55b20ed7997f37b0c72b21c236c93d9def2
Saad <124282322+adnanadilalpha@users.noreply.github.com>
2023-04-11 16:04:46 +0500

    another bug fixed

--- pages/components/people/component/begin.jsx
+++ pages/components/people/component/begin.jsx
@@ -7,7 +7,7 @@ const section = () => {
         <div className="point mt-16 xl:mt-36 mb-44">
         <div className="w-full flex flex-col padding">
                 <p className="font text-zinc-400 md:text-base  text-sm xl:text-2xl font-thin uppercase">In the beginning</p>
-                <p className="Basement text-base md:text-xl xl:text-2xl">
+                <p className="Basement text-base md:text-xl xl:text-2xl font-extrabold">
                     Two guys.
                     Infitine cups of coffee.
                     A couple of laptops and a sloppy internet connection.
@@ -16,7 +16,7 @@ const section = () => {
     
         <div className="w-full flex flex-col padding--2">
             <p className="font text-zinc-400 md:text-base  text-sm xl:text-2xl font-thin uppercase">In the Middle</p>
-            <p className="Basement text-base md:text-xl xl:text-2xl">
+            <p className="Basement text-base md:text-xl xl:text-2xl font-extrabold">
             Two guys.
 Plus more guys.
 Plenty of cureveballs, lots of learning.
@@ -26,7 +26,7 @@ Some screw-ups along the way.
     
     <div className="w-full flex flex-col padding--3">
         <p className="font text-zinc-400 md:text-base  text-sm xl:text-2xl font-thin uppercase">Not the end</p>
-        <p className="Basement text-base md:text-xl xl:text-2xl">
+        <p className="Basement text-base md:text-xl xl:text-2xl font-extrabold">
 Lean team.
 A relationship-first agency.
 Friends with Fortune 500.
