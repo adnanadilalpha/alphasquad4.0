1797286a34310c8f7a998022d6ab8560beb60b67
Saad <124282322+adnanadilalpha@users.noreply.github.com>
2023-04-11 15:55:52 +0500

    the people page done

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ export default function Document() {
     <Html lang="en" className='bg-black'>
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-<link href="https://fonts.googleapis.com/css2?family=Antonio:wght@700&family=Archivo:wght@400;700&display=swap" rel="stylesheet" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Archivo:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
