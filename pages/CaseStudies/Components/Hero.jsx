8216e21e5c14aaa09c04b8c91db595450943d8e4
Saad <124282322+adnanadilalpha@users.noreply.github.com>
2023-04-17 13:38:48 +0500

    bug fixed

--- pages/CaseStudies/Components/Hero.jsx
+++ pages/CaseStudies/Components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = (prop) => {
   return (
 <div className="flex justify-center">
-    <div className="flex justify-center mb-32">
+    <div className="flex justify-center mb-12 xl:mb-32">
     <img src={prop.img} alt="" />
     </div>
 </div>
